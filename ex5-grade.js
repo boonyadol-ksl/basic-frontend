c7bbe80a4027f6b903b4abb778b599bf878e5e6a
boonyadol-ksl <142763046+boonyadol-ksl@users.noreply.github.com>
2025-06-28 15:55:59 +0700

    Update ex5-grade.js

--- ex5-grade.js
+++ ex5-grade.js
@@ -1,14 +1,19 @@
 const calculateGrade = (score) => {
-  if (score >= 80) return "A";
-  else if (score >= 75) return "B+";
-  else if (score >= 70) return "B";
-  else if (score >= 65) return "C+";
-  else if (score >= 60) return "C";
-  else if (score >= 55) return "D+";
-  else if (score >= 50) return "D";
-  else return "F";
+    if (score >= 80) return "A";
+    else if (score >= 75) return "B+";
+    else if (score >= 70) return "B";
+    else if (score >= 65) return "C+";
+    else if (score >= 60) return "C";
+    else if (score >= 55) return "D+";
+    else if (score >= 50) return "D";
+    else return "F";
 };
 
 
 console.log(calculateGrade(76)); // A
 console.log(calculateGrade(55)); // B+
+
+
+const calculateRectangleArea = (a, b) => a * b;
+console.log(calculateRectangleArea(10, 5)); // A
+console.log(calculateRectangleArea(7, 3)); // B+
